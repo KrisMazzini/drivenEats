35818accb67813823768345f7519968fede336f3
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2021-10-30 16:53:41 -0300

    Rodar teste para entrega

--- drivenEats.js
+++ drivenEats.js
@@ -1,13 +1,24 @@
 let pedido = {prato: null, bebida: null, sobremesa: null};
 
+let solicitante, endereco;
+
 const botao = document.querySelector('.botao');
 const opcoes = document.querySelectorAll('.opcao');
+const confirmar = document.querySelector('.ok');
+const cancelar = document.querySelector('.cancelar');
+const validacao = document.querySelector('.novaPagina');
+const body = document.querySelector('body');
+const total = document.querySelector('.total');
 
 botao.onclick = function() {finalizarPedido()};
 
 opcoes.forEach (op => {
-    op.onclick = function() {selecionarOpcao(this)}
+    op.onclick = function() {selecionarOpcao(this)};
 })
+
+confirmar.onclick = function() {confirmarPedido()};
+
+cancelar.onclick = function() {cancelarPedido()};
 
 // -------------------- FUNCTIONS --------------------
 
@@ -15,40 +26,72 @@ function selecionarOpcao(opcao) {
     const tipo = opcao.parentElement.parentElement.classList[0];
     const selecionada = document.querySelector(`.${tipo} .selecionada`);
 
-    const descricao = opcao.children.namedItem('textoOpcao');
-    const nome = descricao.children.namedItem('nome').innerHTML;
-    const preco = Number(descricao.children.namedItem('preco').innerHTML.replace('R$ ', '').replace(',', '.'));
+    const resumo_nome = document.querySelector(`#resumo_${tipo} .nome`);
+    const resumo_preco = document.querySelector(`#resumo_${tipo} .preco`);
 
     if (selecionada && selecionada !== opcao) {
         selecionada.classList.remove('selecionada');
     }
 
     opcao.classList.toggle('selecionada');
+    
+    if (opcao.classList.contains('selecionada')) {
+        const nome = document.querySelector(`.${tipo} .selecionada .nome`).innerHTML;
+        const preco = Number(document.querySelector(`.${tipo} .selecionada .preco`).innerHTML.replace('R$ ', '').replace(',', '.'));
 
-    pedido[tipo] = opcao.classList.contains('selecionada') ? {nome: nome, preco: preco} : null;
+        pedido[tipo] = {nome: nome, preco: preco};
+        resumo_nome.innerHTML = nome;
+        resumo_preco.innerHTML = preco.toFixed(2).toString().replace('.', ',');
+    } else {
+        pedido[tipo] = null;
+        resumo_nome.innerHTML = tipo;
+        resumo_preco.innerHTML = '0,00';
+    }
 
     if (pedido && pedido.prato && pedido.bebida && pedido.sobremesa){
         botao.classList.add('finalizar');
+        botao.innerHTML = 'Fechar pedido'
     } else {
         botao.classList.remove('finalizar');
+        botao.innerHTML = 'Selecione os 3 itens<br>para fechar o pedido'
     }
 }
 
 function finalizarPedido() {
     if (botao.classList.contains('finalizar')) {
-        const nome = prompt('Qual o seu nome?');
-        const endereco = prompt('Qual o seu endereço?');
+        solicitante = prompt('Qual o seu nome?');
+        endereco = prompt('Qual o seu endereço?');
 
-        const total = (pedido.prato.preco + pedido.bebida.preco + pedido.sobremesa.preco).toFixed(2).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
-
-        const mensagem = encodeURIComponent('Olá, gostaria de fazer o pedido:\n' + 
-                        `- Prato: ${pedido.prato.nome}\n` + 
-                        `- Bebida: ${pedido.bebida.nome}\n` + 
-                        `- Sobremesa: ${pedido.sobremesa.nome}\n` + 
-                        `Total: ${total}\n\n` +
-                        `Nome: ${nome}\n` +
-                        `Endereço: ${endereco}`);
-
-        window.open("https://wa.me/5532988248479?text=" + mensagem);
+        exibirConfirmacao();
     }
+}
+
+function exibirConfirmacao() {
+    body.style.overflow = "hidden";
+    validacao.style.display = "flex";
+    
+    total.innerHTML = `<span>TOTAL</span>\n<span>${calculaTotal()}</span>`;
+}
+
+function confirmarPedido() {
+    const mensagem = encodeURIComponent('Olá, gostaria de fazer o pedido:\n' + 
+                                        `- Prato: ${pedido.prato.nome}\n` + 
+                                         `- Bebida: ${pedido.bebida.nome}\n` + 
+                                        `- Sobremesa: ${pedido.sobremesa.nome}\n` + 
+                                         `Total: ${calculaTotal()}\n\n` +
+                                         `Nome: ${solicitante}\n` +
+                                         `Endereço: ${endereco}`);
+
+    window.open("https://wa.me/5532988248479?text=" + mensagem);
+}
+
+function cancelarPedido() {
+    body.style.overflow = "auto";
+    validacao.style.display = "none";
+
+    total.innerHTML = `<span>TOTAL</span>\n<span>R$ 0,00</span>`;
+}
+
+function calculaTotal() {
+    return 'R$ ' + (pedido.prato.preco + pedido.bebida.preco + pedido.sobremesa.preco).toFixed(2).toString().replace('.', ',');
 }
